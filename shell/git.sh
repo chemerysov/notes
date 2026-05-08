e0759d0fddfbad48f2c08a389fadf2596a6d77fe
Andrii Chemerysov <achemerysov@gmail.com>
2026-05-07 22:50:05 -0400

    fix: git notes fixed

--- shell/git.sh
+++ shell/git.sh
@@ -12,30 +12,26 @@ git init
 
 git clone https://github.com/chemerysov/os.git
 
+git add .
+git commit -m "initial commit"
+git remote add origin https://github.com/chemerysov/notes.git
+git push -u origin main
+git push
+
 git status
 git branch -m main
 
-git add .
-git commit -m "initial commit"
 git commit --amend -m "amend"
-
-git remote add origin https://github.com/chemerysov/os.git
-git push -u origin main
-
-git push
 
 git tag -a v0.0.1 -m "something"
 git push -u origin v0.0.1
 
 
 git checkout -b feat/something
-
 git add .
 git commit -m "yet another commit"
-
-git push -u origin feat/something
-git push
-
+git push origin feat/something
 git checkout main
-git pull
 git branch -D feat/something
+
+git pull
